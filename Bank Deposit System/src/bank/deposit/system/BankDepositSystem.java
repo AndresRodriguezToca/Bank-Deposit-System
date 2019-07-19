9ed64aed5da428676a6f90f4fe752e74f3832f0f
Andres rodriguez Toca <48394901+AndresRodriguezToca@users.noreply.github.com>
2019-07-19 14:25:05 -0400

    Add files via upload

---
+++ Bank Deposit System/src/bank/deposit/system/BankDepositSystem.java
@@ -0,0 +1,16 @@
+package bank.deposit.system;
+
+import Frames.mainFrame;
+
+
+public class BankDepositSystem {
+
+    
+    public static void main(String[] args) {
+    
+    mainFrame loginFrame = new mainFrame();
+    loginFrame.setVisible(true);
+    
+    }
+    
+}
